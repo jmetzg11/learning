5aabc6408f77b0b55c4daf77cc0d9d774218dd73
jmetzg11 <jmetzg11@gmail.com>
2026-04-07 21:40:03 -0500

    Started mistake 25 in go

--- 100_Go_Mistakes/3_data_types/25_unexpected_side_effects_using_slice_append.go
+++ 100_Go_Mistakes/3_data_types/25_unexpected_side_effects_using_slice_append.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s1 := []int{1, 2, 3}
+	s2 := s1[1:2]
+	s3 := append(s2, 10)
+	fmt.Printf("slice 1: %v, slice 3: %v\n", s1, s3)
+	// s1 was modified by the append
+	// slice 1: [1 2 10], slice 3: [2 10]
+
+	s := []int{1, 2, 3}
+	sCopy := make([]int, 2)
+	copy(sCopy, s)
+	f(sCopy)
+	fmt.Printf("original: %v, copy: %v\n", s, sCopy)
+}
+
+func f(s []int) {
+	_ = append(s, 10)
+}
