6e3b955cc8b57c4f7262e2e3776a2ddec569c2a4
jmetzg11 <jmetzg11@gmail.com>
2026-04-21 21:34:17 -0500

    Finish mistake 51

---
+++ 100_Go_Mistakes/7_error_management/51_checking_an_error_value_inaccurately.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+)
+
+// Create an error value -> sentinel error
+var ErrFoo = errors.New("foo")
+
+func main() {
+	err := query()
+	if err != nil {
+		// will always return false if sql.ErrorNoRows is wrapped in fmt.Errorf with the % w directive
+		if err == sql.ErrNoRows {
+			// do something from sentinel error
+		} else {
+			// unexpected error, handle differently
+		}
+	}
+
+	// Solution
+	err := query()
+	if err != nil {
+		if errors.Is(err, squlErrNoRows) {
+			// do something from sentinel error
+		} else {
+			// unexpected error, handle differently
+		}
+	}
+}
+
+// errors.Is() and error.As() will unwrap the error
